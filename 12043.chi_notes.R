c30c9cd5569af3d4a895166cca799c72129306ab
esteeschwarz <gith@stschwarz.dialup.fu-berlin.de>
2022-01-24 21:07:31 +0100

    12043.6

--- 12043.chi_notes.R
+++ 12043.chi_notes.R
@@ -23,11 +23,15 @@ pd<-0.25
 e<-c(a,b,c,d)
 f<-c(pa,pb,pc,pd)
 
+pra<-(prop.table(e)*100)
+paa<-sum (pra[1:2])
+pbb<-sum (pra[3:4])
+p3<-c(paa,pbb)
 chi1<-cbind(e)
-chisq.test(chi1,p=f,correct=FALSE)
+chisq.test(chi1,p=pra,correct=FALSE)
 print("-------------------------")
-
-
+57.5+17.5+17.5+7.5
+#sqrt(11*12)
 #datensatz rosemeyer übung
 #sgen<-c(10,30)
 #ofgen<-c(40,20)
@@ -45,31 +49,62 @@ sgen<-c(125,64) #spalte 1
 ofgen<-c(69,145) #spalte 2
 #rose: rbind
 #hoch signifikant, wir können die nullhypothese ablehnen
-#sgen 22,18
-#ofgen 28,32
-psgen<-22+18
-pofgen<-28+32
-liv<-c("animate","inanimate") # zeilen
+#p sgen 22,18
+#p ofgen 28,32
+#psgen<-22+18
+#pofgen<-28+32
+#liv<-c("animate","inanimate") # zeilen
 chi2<-cbind(sgen,ofgen)
-round(prop.table(chi2)*100)
+pr1<-round(prop.table(chi2)*100)
+psgen<-sum (pr1[1:2])
+pofgen<-sum (pr1[3:4])
+
 #spaltensumme * zeilensumme/ n
 #22-18o 28-32u
 #wahrscheinlichkeiten summe 40/60
 # p (sgen, fgen)
 f2<-c(psgen,pofgen)
-chisq.test(chi2,correct=FALSE)
-#ohne probability angabe auch möglich
+chisq.test(chi2,p=f2,correct=FALSE)
+#ohne probability angabe -f2- auch möglich
 print("-------------------------")
 
 ##meindl uebung U test, set C
-
-#stichprobe 1,2
+#################
+#################
+#################
+#hier ist der teil, in dem ich krampfhaft versuche, den 
+#von meindl angenommenen standardfehler [12]
+#der stichporben des U-test beispiels
+#zu berechnen. vielleicht können Sie mir helfen...
+#ich habe das ganze buch auf varianz, standardfehler usw.
+#durchgearbeitet. niemals komme ich auf 12.
+#################################
+#################################
+#stichprobe meindl u-test übung
 cst1<-c(22, 22, 29, 30, 32, 30, 21, 29, 28, 29, 25)
 cst2<-c(21, 25, 20, 22, 20, 22, 20, 23, 25, 28, 23)
 
+
+mn3<-mean(c3)
 c3<-c(cst1,cst2)
+sqrt(sd(c3)/22)
+csd<-sd(c3)
+(sum(c3-mn3)^2)/21
+sd3<-(c3-mn3)^2
+varp3<-sum(sd3[1:22])/21
+sqrt(varp3/22)
+(csd*csd)/20
+vr3<-var(c3)
+(vr3*vr3)/21
+############################
+############################
+############################
+
+
+
 c5<-rank(c3)
 
+wilcox.test(cst1-cst2,correct=FALSE)
 #sum ranking
 c6<-sum(c5[1:11])
 c7<-sum(c5[12:22])
@@ -83,6 +118,8 @@ d1<-c(7, 10, 12, 12, 18,  6 , 6, 18, 18, 12,  7 ,17,  9,  6 ,16)
 d2<-c(17,  5, 13, 20,  6,  6,  9, 12, 18,  7, 14, 17, 17,  6, 18)
 d3<-c(d1,d2)
 #wilcox test
+wilcox.test(d3)
+wilcox.test(d1,d2,alternative="greater")
 d5<-rank(d3)
 d4<-cbind(d5,d3)
 d6<-sum(d5[1:15])

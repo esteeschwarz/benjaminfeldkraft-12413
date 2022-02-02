6bb1ff42ba1733fe9573fc3a3ab6ffd9a5c3f527
esteeschwarz <gith@stschwarz.dialup.fu-berlin.de>
2022-02-02 11:55:15 +0100

    2

--- cp_loop.r
+++ cp_loop.r
@@ -9,19 +9,21 @@ print(la)
 k<-0
 simv<-c()
 sim1<-a1[k]
-
-sim2<-c(append(simv,sim1))
-sim3<-function(k) {print("k") 
-print(a1[k])}
-print(sim2)
-for(k in la){
-	ifelse(a1[k]==a2[k],sim3,sim1<-1)
+aempty<-c(1:la)
+aempty<-aempty*0
+#sim2<-c(append(simv,sim1))
+#sim3<-function(k) {print("k") 
+#print(a1[k])}
+#print(sim2)
+pos1<-0
+for(k in 1:la){
+	ifelse(a1[k]==a2[k],pos1<-k,pos1<-0)
 	#sim1<-c(a1[sim])
-print (sim1)
+print (pos1)
 print("siblins")
-print (sim2)
+print (a1[pos1])
 #sim2<-append(simv,sim1,after=k)
-sim<-0
+pos1<-0
 }
 print(sim2)
 
@@ -29,17 +31,25 @@ print(sim2)
 ##works halfway
 i<-0
 b<-5
+b0<-c()
 b1<-c(1:3)
 b2<-c("aa","bb")
 b5<-c(b1,b2)
+lb5<-length(b5)
 
 b3<-function(b) {
+  b0<-c()
+  
+  #  c<-b+1
+  print(c)
 for (k in 1:b){
-print (b1)
-b9<-append(b1,b5,after=6)
+print(k)
+  print (b1)
+b9<-append(b0,b5,after=k)
 print(b9)  
 
 }
+  print(b9)
   }
 
 
@@ -47,10 +57,54 @@ print(b9)
 
 b3(4)
 ################
-c0<-1:8
+cs<-"kk"
+f2<-function(z){
+f1<- function(x) {
+  for (k in 1:x) {
+    y<-cs2<-c(cs,cs)
+    
+    print(cs2)
+    }
+return(y)
+  }
+
+
+cs3<-f1(4)
+}
+print(cs3)
+cs4<-f2(3)
+print (cs4)
+print (f2)
+###########
+#function base
+
+z<-(function(x, y)
+  { z <- x^2 + y^2;x+y+z }
+#2,1: 4,2;4+2+2 =8
+   )(1:8, 1)
+
+
+print(z)
+
+z<-function(x,y)
+{z<- c(x,y,x,y)}
+
+  
+
+print(z(1,3))
+
+
+##############
+
+
+
+c0<-1:9
 lc0<-length(c0)
+lc0=2
+
 #append after end
-append(c0, 1:4, after=lc0)
+#after darf nicht gröszer sein als länge vector
+append(c0, 1:10, after=lc0)
   
 
 

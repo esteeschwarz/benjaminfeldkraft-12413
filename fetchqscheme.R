dac4e9a9b534fe2f3dec690cd677c8acfd350355
esteeschwarz <gith@stschwarz.dialup.fu-berlin.de>
2022-02-05 14:05:42 +0100

    fetchq

--- fetchqscheme.R
+++ fetchqscheme.R
@@ -1,1 +1,1 @@
-download.file("https://github.com/esteeschwarz/essais/blob/main/12057.qscheme_essai.R","qscript.R")
+download.file("https://github.com/esteeschwarz/essais/raw/main/12057.qscheme_essai.R","qscript.R")

2c0f8271092c152a437ae049c559dd1b8155cc9f
esteeschwarz <gith@stschwarz.dialup.fu-berlin.de>
2022-02-06 11:39:47 +0100

    mach...

---
+++ docs/w3school/R/12062.notes.R
@@ -0,0 +1,21 @@
+#12061.
+#qscheme computations
+
+#element <question> within template:
+55
+#mal anzahl items
+qsection<-(55*64)
+plus<-19
+#3456 lines for whole of questions
+#17 lines befor, 2 lines after question elements
+print(doclength<-qsection+plus)
+print (qsection)
+print(doclength)
+cptplus<-4199-doclength-plus
+itemssc<-(actplus-plus)/54
+now<-(4199-19)/55
+#76 items
+torem<-now-64
+#test
+torem*55+64*55+19
+#12 items to remove

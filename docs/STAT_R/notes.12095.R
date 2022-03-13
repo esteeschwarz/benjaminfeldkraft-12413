f9eb478e91656d22802a49c32df6209524ba0106
esteeschwarz <gith@stschwarz.dialup.fu-berlin.de>
2022-03-13 18:38:40 +0100

    upd

--- docs/STAT_R/notes.12095.R
+++ docs/STAT_R/notes.12095.R
@@ -14,3 +14,16 @@ tx<-readline()
 txa<-as.double(unlist(strsplit(tx,",")))
 txa[1]+txa[2]
 cat("helo",txa)
+
+
+## From Agresti(2007) p.39
+M <- as.table(rbind(c(740, 327, 468), c(484, 239, 477)))
+M
+dimnames(M) <- list(gender = c("F", "M"),
+                    
+                    party = c("Democrat","Independent", "Republican"))
+(Xsq <- chisq.test(M))  # Prints test summary
+Xsq$observed   # observed counts (same as M)
+Xsq$expected   # expected counts under the null
+Xsq$residuals  # Pearson residuals
+Xsq$stdres     # standardized residuals

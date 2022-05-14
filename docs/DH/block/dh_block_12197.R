d4b1045a38cd1baa37c1946f084b096138d1be47
esteeschwarz <gith@stschwarz.dialup.fu-berlin.de>
2022-05-14 23:29:47 +0200

    upd

--- docs/DH/block/dh_block_12197.R
+++ docs/DH/block/dh_block_12197.R
@@ -5,28 +5,30 @@ library(stylo)
 #corpus<-"..data/corpora/benjamin"
 #corpuscpt<-"..data/corpora/grimm-kontrastanalyse/corpus"
 
+setup<-function(){
 corpus2<-"~/PRO/git/essais/docs/DH/block/corpora/grimm-kontrastanalyse/corpus/primary_set"
 corpus1<-"~/PRO/git/essais/docs/DH/block/corpora/grimm-kontrastanalyse/corpus/secondary_set"
 corpus1<-"~/PRO/git/essais/docs/DH/block/corpora/benjamin/kontrast/2"
 corpus2<-"~/PRO/git/essais/docs/DH/block/corpora/benjamin/kontrast/3"
 
-work<-"~/PRO/git/essais/docs/DH/block/temp"
+}
 #work<-"~/PRO/git/essais/docs/DH/block/temp"
 #stylo(corpus.dir = corpus, path=work, gui = T)
-comp_e<-oppose(primary.corpus.dir=corpus1,secondary.corpus.dir=corpus2,path=work)
+#comp_e<-oppose(primary.corpus.dir=corpus1,secondary.corpus.dir=corpus2,path=work)
 #funktionsfehler: setzt WD nicht von R WD aus, also path absolut bestimmen
-comp$comparison
-library(stringi)
+#comp$comparison
 
-stri_detect_regex()
+#library(stringi)
+
+#stri_detect_regex()
 ###################
-src<-"/Users/guhl/PRO/git/clones/forTEXT.net/Preprocessing mit NLTK/Werther_Saetze_neu1.txt"
-txt<-scan(src," ")
-regex<-"(.{5,20}Werther)(.{5,20})"
-regex="Werther"
-kwic<-grep(pattern=regex,txt,value=T)
+#src<-"/Users/guhl/PRO/git/clones/forTEXT.net/Preprocessing mit NLTK/Werther_Saetze_neu1.txt"
+#txt<-scan(src," ")
+#regex<-"(.{5,20}Werther)(.{5,20})"
+#regex="Werther"
+#kwic<-grep(pattern=regex,txt,value=T)
 #write_csv(kwic,"/Users/guhl/PRO/git/clones/forTEXT.net/Preprocessing mit NLTK/rgexout.txt")
-print(kwic)
+#print(kwic)
 ###########
 #corpus cite:
 #Rauh, Christian; Schwalbach, Jan, 2020, 
@@ -38,12 +40,14 @@ print(kwic)
 #src<-"//dataverse.harvard.edu/file.xhtml?persistentId=doi:10.7910/DVN/L4OAKN/PCYUNY&version=1.0#"
 #con<-gzfile(src)
 #dta<-unz("https",filename=src,open = "r")
+
+work<-"~/PRO/git/essais/docs/DH/block/temp"
 wd<-"/Users/guhl/Nextcloud/UNI/21S/DH/"
 src<-"/Users/guhl/Nextcloud/UNI/21S/DH/data/corpus/BT/Corp_Bundestag_V2.rds"
 dta<-readRDS(src)
 colnames(dta)
 party<-unique(dta$party)
-sum(dta$party=="SPD",na.rm = T)
+#sum(dta$party=="SPD",na.rm = T)
 library(stringi)
 mean(stri_count_words(dta$text))
 #265
@@ -81,10 +85,10 @@ for (row in 1:length(partyns)){
 #  stylotext <- select(dta, text)
   write.table(stylotext, file = paste0(path2, bookname,".txt"), sep="", row.names = FALSE)
 }
+cat("files written")
+#stylotext <- select(dta, text)
+#write.table(stylotext, file = "data/hegel.txt", sep="", row.names = FALSE)
 
-stylotext <- select(dta, text)
-write.table(stylotext, file = "data/hegel.txt", sep="", row.names = FALSE)
-
-corpus[1,1]
+#corpus[1,1]
 
 

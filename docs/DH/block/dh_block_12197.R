b1a18b99f9a89fe21f8ccf13b04740d0d695e5b2
esteeschwarz <gith@stschwarz.dialup.fu-berlin.de>
2022-05-14 08:52:45 +0200

    upd BT

--- docs/DH/block/dh_block_12197.R
+++ docs/DH/block/dh_block_12197.R
@@ -38,13 +38,53 @@ print(kwic)
 #src<-"//dataverse.harvard.edu/file.xhtml?persistentId=doi:10.7910/DVN/L4OAKN/PCYUNY&version=1.0#"
 #con<-gzfile(src)
 #dta<-unz("https",filename=src,open = "r")
+wd<-"/Users/guhl/Nextcloud/UNI/21S/DH/"
 src<-"/Users/guhl/Nextcloud/UNI/21S/DH/data/corpus/BT/Corp_Bundestag_V2.rds"
 dta<-readRDS(src)
 colnames(dta)
+party<-unique(dta$party)
+sum(dta$party=="SPD",na.rm = T)
+library(stringi)
+mean(stri_count_words(dta$text))
+#265
+corp1<-dta$text[dta$party==party[1]]
+corp2<-dta$text[dta$party==party[2]]
+corp3<-dta$text[dta$party==party[3]]
+corp4<-dta$text[dta$party==party[4]]
+corp5<-dta$text[dta$party==party[5]]
+corp6<-dta$text[dta$party==party[6]]
+corp7<-dta$text[dta$party==party[7]]
+corp8<-dta$text[dta$party==party[8]]
+corpus<-rbind(corp1,corp2,corp3,corp4,corp5,corp6,corp7,corp8)
+library(stylo)
+library(dplyr)
+##############
+#lehrjahre1<-c(2335,2336,2337,2338,2339,2340,2341,2342)
+#lehrjahre2<-c("goethe","goethe","goethe","goethe","goethe","goethe","goethe","goethe")
+lehrjahre3<-c("WMeister1","WMeister2","WMeister3","WMeister4","WMeister5","WMeister6","WMeister7","WMeister8")
+lehrjahre4<-c("goethe_WMeister1.txt","goethe_WMeister2.txt","goethe_WMeister3.txt","goethe_WMeister4.txt","goethe_WMeister5.txt","goethe_WMeister6.txt","goethe_WMeister7.txt","goethe_WMeister8.txt")
+partyns<-cbind(party)
+partyns[7]<-"k_A"
+partyns<-cbind("cdu","linke","spd","fdp","gruene","independent","k_a" ,"afd")
+partyns
+###
+path = wd
+dir.create(path, showWarnings = TRUE)
+dir.create(paste0(path, "/temp"))
+dir.create(paste0(path, "/data/corpus/BT/temp"))
 
+path2 <- paste0(wd,"/data/corpus/BT/temp/")
 
+for (row in 1:length(partyns)){
+  bookname <- partyns[row]
+  stylotext <- corpus[,row]
+#  stylotext <- select(dta, text)
+  write.table(stylotext, file = paste0(path2, bookname,".txt"), sep="", row.names = FALSE)
+}
 
+stylotext <- select(dta, text)
+write.table(stylotext, file = "data/hegel.txt", sep="", row.names = FALSE)
 
-
+corpus[1,1]
 
 

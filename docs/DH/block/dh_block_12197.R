d68c78210078f309d2c661cedd9b7d254c655194
esteeschwarz <gith@stschwarz.dialup.fu-berlin.de>
2022-05-14 07:45:16 +0200

    mrn

--- docs/DH/block/dh_block_12197.R
+++ docs/DH/block/dh_block_12197.R
@@ -27,3 +27,24 @@ regex="Werther"
 kwic<-grep(pattern=regex,txt,value=T)
 #write_csv(kwic,"/Users/guhl/PRO/git/clones/forTEXT.net/Preprocessing mit NLTK/rgexout.txt")
 print(kwic)
+###########
+#corpus cite:
+#Rauh, Christian; Schwalbach, Jan, 2020, 
+#"The ParlSpeech V2 data set: Full-text corpora of 6.3 million 
+#parliamentary speeches in the key legislative chambers of nine representative democracies", 
+#https://doi.org/10.7910/DVN/L4OAKN, Harvard Dataverse, V1
+###
+#src<-"//dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/L4OAKN/PCYUNY"
+#src<-"//dataverse.harvard.edu/file.xhtml?persistentId=doi:10.7910/DVN/L4OAKN/PCYUNY&version=1.0#"
+#con<-gzfile(src)
+#dta<-unz("https",filename=src,open = "r")
+src<-"/Users/guhl/Nextcloud/UNI/21S/DH/data/corpus/BT/Corp_Bundestag_V2.rds"
+dta<-readRDS(src)
+colnames(dta)
+
+
+
+
+
+
+
